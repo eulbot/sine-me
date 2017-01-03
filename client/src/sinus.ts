471785af928dcff6edc4d4db4567302b99cbd117
Ulrich Fischer <careforthegoodones@gmail.com>
2017-01-03 19:25:00 +0100

    Revised sine transformation

--- client/src/sinus.ts
+++ client/src/sinus.ts
@@ -17,13 +17,14 @@ export class Sinus {
     private ctx: CanvasRenderingContext2D;
     private data: number[][];
     private sines: number[][];
-    private plot: number[][];
     private ps: number;
 
     private easing: Easing[];
     private immediate: boolean;
     private sf: number;
-    private ef: number = 0;
+    private ef: number;
+    private fx: number;
+    private max: number;
 
     constructor (public canvas: HTMLCanvasElement) { 
         this.ctx = canvas.getContext('2d');
@@ -51,32 +52,25 @@ export class Sinus {
 
     private calcSines = () => {
 
-        this.plot = [];
-        this.sines = new Array(this.data.length);
+        this.sines = [];
         let w = this.canvas.width, h = this.canvas.height;
         let xs = _.range(0, this.data[0].length).map((x) => Math.round(w * x / (this.data[0].length - 1)));
-        let step = xs.length / w;
 
         for(let row = 0; row < this.data.length; row++) {
-            let dent = this.data[row].map((x) => this.ps - (x * this.ps / 255));
-            this.sines[row] = spline.getCurvePoints(_.flatten(_.zip(xs, dent)), .5, this.ps);
-            this.plot[row] = [];
+            this.sines[row] = [];
+            let splined: Array<number> = spline.getCurvePoints(_.flatten(_.zip(xs, this.data[row])), .5, this.ps * this.fx);
+            let offset = (row * this.ps) + this.ps / 2;
+            let phase = this.r(0, 180);
 
-            let phase = 0;
-            for(let i = 0; i < this.sines[row].length; i++) {
+            for (let i = 0; i < splined.length - 1; i += 2) {
 
-                if(i % 2 == 0) {
+                let delta = i > 0 ? splined[i] - splined[i - 2] : 0;
+                let pace = Math.abs(this.max - splined[i + 1]);
+                phase += (delta * (pace / Math.PI));
+                let y = Math.sin(phase * (Math.PI / 180));
 
-                    let offset = (row * this.ps) + this.ps / 1.5;
-                    let delta = i > 0 ? this.sines[row][i] - this.sines[row][i - 2] : 0;
-                    let fqcy = this.sines[row][i + 1];
-                    let amount = (1 - (this.ps - fqcy) / this.ps);
-
-                    phase += delta * amount;
-                    let y = Math.sin(phase) * this.ps / 2 * Math.pow(amount, 2);
-                    this.plot[row][i] = this.sines[row][i];
-                    this.plot[row][i + 1] = y + offset;
-                }
+                this.sines[row].push(splined[i] * this.fx);
+                this.sines[row].push(y * (this.ps * Math.sqrt(pace / this.max) / 2) + offset);
             }
         }   
 
@@ -88,23 +82,24 @@ export class Sinus {
         let h = this.canvas.height;
         this.ctx.beginPath();
         this.sf = this.ef;
-        this.ef += 4;
-        
-        for (let i = 0; i < this.plot.length; i++) {
+        this.ef += this.fx;
 
-            let row = this.plot[i], er = this.easing[i];
-            let from = this.immediate ? 0 : 2 * Math.round(er.func(this.sf / w, er.ll, er.ul) * w / 2);
-            let to = this.immediate ? w : 2 * Math.round(er.func(this.ef / w, er.ll, er.ul) * w / 2);
-            let r = er.direction * (this.plot[i].length / 2 - 1);
+        for (let i = 0; i < this.sines.length; i++) {
 
-            this.ctx.moveTo(this.plot[i][Math.abs(r - from) * 2], (this.plot[i][Math.abs(r - from) * 2 + 1]));
-            for(let x = from + 2; x <= to && x < row.length; x++) 
-                this.ctx.lineTo(row[Math.abs(r - x) * 2], row[Math.abs(r - x) * 2 + 1]);
+            let row = this.sines[i], er = this.easing[i];
+            let from = this.immediate ? 0 : Math.round(er.func(this.sf / w, er.ll, er.ul) * w * this.fx);
+            let to = this.immediate ? w * this.fx : Math.round(er.func(this.ef / w, er.ll, er.ul) * w * this.fx);
+            let r = er.direction * (this.sines[i].length / 2 - 1);
+
+            this.ctx.moveTo(this.sines[i][Math.abs(r - from) * 2] / this.fx, (this.sines[i][Math.abs(r - from) * 2 + 1]));
+            for(let x = from + 2; x <= to; x++) 
+                this.ctx.lineTo(row[Math.abs(r - x) * 2] / this.fx, row[Math.abs(r - x) * 2 + 1]);
+                
         }
 
         this.ctx.stroke();
 
-        if(!this.immediate && this.ef <= this.plot[0].length + this.ef / 2) {
+        if(!this.immediate && Math.abs(this.ef - this.sines[0].length + this.ef / 2) > 0) {
             setTimeout(() => {
                 window.requestAnimationFrame(this.draw);
             }, 1000 / 48);
@@ -112,10 +107,17 @@ export class Sinus {
     }
 
     private setupCanvas = () => {
-        this.ctx.canvas.width = $(window).width();
+
+        this.ef = 0;
+        this.fx = 4;
+        this.max = 255;
+
+        this.ctx.canvas.width = $(window).outerWidth();
         this.ctx.canvas.height = this.data.length * this.ps;
-        this.ctx.lineWidth = 2.6;
+        this.ctx.lineWidth = 1;
         this.ctx.strokeStyle = 'rgba(0, 0, 0, 0.95)';
+        this.ctx.shadowBlur = 2.5;
+        this.ctx.shadowColor = 'rgb(0, 0, 0)';
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 
@@ -124,7 +126,7 @@ export class Sinus {
         let lowerLimit, upperLimit;
         this.easing = [];
 
-        for(let i = 0; i < this.plot.length; i++) {
+        for(let i = 0; i < this.sines.length; i++) {
             
             lowerLimit = this.r(0, .5);
             upperLimit = this.r(0, .5 - lowerLimit);
@@ -150,12 +152,12 @@ export class Sinus {
 
     private easeInQuad: EaseFunc = (t: number, ll: number, ul: number): number => {
         
-        return t < ll ? 0 : t >= 1 - ul ? 1 : Math.pow((t - ll) * 1 / (1 - ll - ul), 2)
+        return t < ll ? 0 : t > 1 ? 1 : t >= 1 - ul ? 1 : Math.pow((t - ll) * 1 / (1 - ll - ul), 2)
 	}
 
 	private easeOutQuad: EaseFunc = (t: number, ll: number, ul: number): number => {
 
-        return t < ll ? 0 : t >= 1 - ul ? 1 : (t - ll) * 1 / (1 - ll - ul) * (2 - (t - ll) * 1 / (1 - ll - ul));
+        return t < ll ? 0 : t > 1 ? 1 : t >= 1 - ul ? 1 : (t - ll) * 1 / (1 - ll - ul) * (2 - (t - ll) * 1 / (1 - ll - ul));
 	}
     
 	private easeInOutQuad: EaseFunc = (t: number, ll: number, ul: number): number => {
